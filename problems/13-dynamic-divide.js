c8c7a88e82dccfb4a21154bfbc64ff01fd69a735
John Carlo Lipa <johncarlolipa@gmail.com>
2024-01-13 23:21:50 +0800

    done exercie 14

--- problems/13-dynamic-divide.js
+++ problems/13-dynamic-divide.js
@@ -20,6 +20,9 @@ divideByFive(50); // returns 10
 
 function dynamicDivide(divisor) {
   // Your code here
+  return function(...arguments){
+    return arguments[0] / divisor;
+  }
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
