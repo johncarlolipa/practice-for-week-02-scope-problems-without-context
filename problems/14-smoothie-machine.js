f8c4c3f04539f11d82c7f249db48fdd1e09d7cce
John Carlo Lipa <johncarlolipa@gmail.com>
2024-01-14 07:56:22 +0800

    done exercie 15

--- problems/14-smoothie-machine.js
+++ problems/14-smoothie-machine.js
@@ -4,7 +4,7 @@ and returns a function.
 
 The returned function will also accept any number of parameters and will
 return a string including all of the parameters of smoothieMachine as well
-as the returned function's parameters. Look at the examples for a guide of 
+as the returned function's parameters. Look at the examples for a guide of
 how your return should be formatted!
 
 See below for examples:
@@ -23,8 +23,12 @@ console.log(smoothie2("pineapple"));
 // prints "I'm having a smoothie with apples and bananas and berries and pineapple"
 ***********************************************************************/
 
-const smoothieMachine = (...ingredients) => {
-  // Your code here
+const smoothieMachine = (...initialIngredients) => {
+  return function(...additionalIngredients){
+    const allIngredients = [...initialIngredients, ...additionalIngredients]
+    const ingredientList = allIngredients.join(' and ')
+    return `I'm having a smoothie with ${ingredientList}`;
+  }
 };
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
