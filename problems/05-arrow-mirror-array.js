4f1b32c7cf22c6fbc2e8278a755c5076fcd7076b
John Carlo Lipa <johncarlolipa@gmail.com>
2024-01-13 19:38:11 +0800

    done exercie 6

--- problems/05-arrow-mirror-array.js
+++ problems/05-arrow-mirror-array.js
@@ -10,6 +10,11 @@ mirrorArray(['a', 'b', 'c', 'd']); // => [ 'a', 'b', 'c', 'd', 'd', 'c', 'b', 'a
 
 const mirrorArray = (array) => {
   // Your code here
+  let newArr = [...array]
+  for (let i = array.length - 1; i >= 0; i--){
+    newArr.push(array[i])
+  }
+  return newArr
 };
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
